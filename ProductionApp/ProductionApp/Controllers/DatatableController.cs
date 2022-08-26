a1f90c9d552e46467a0286c780e53de069318ca9
AMINUR <aminur6264@gmail.com>
2022-08-26 14:56:07 +0600

    26082022 Old COmmit

---
+++ ProductionApp/ProductionApp/Controllers/DatatableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductionApp.Controllers
+{
+    public class DatatableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
